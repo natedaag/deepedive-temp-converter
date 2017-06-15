694877bc34b99e5510bb590fc091eb9f9a0f69df
natedaag <natedaage@yahoo.com>
2017-06-15 09:27:53 -0600

    created F2C and ran

---
+++ src/edu/cnm/deepdive/temperature/F2C.java
@@ -0,0 +1,30 @@
+package edu.cnm.deepdive.temperature;
+
+public class F2C {
+
+	public static final String USAGE_MESSAGE = "Usage: java %s tempFahrenheit%n%n"
+			+ "where tempCelsius is temperature in Fahrenheit degrees";
+	public static final String OUTPUT_MESSAGE = "%8.3f\u00B0 Fahrenheit = %8.3f\u00B0 Celsius";
+
+	public static void main(String[] args) {
+
+		if (args.length > 0) {
+			try {
+				double f = Double.parseDouble(args[0]);
+				double c = (f - 32) * 5 / 9;
+				System.out.printf(OUTPUT_MESSAGE, f, c);
+			} // end try clause
+			catch (NumberFormatException ex) {
+				ex.printStackTrace();
+				System.out.printf(USAGE_MESSAGE, F2C.class.getName());
+			} // end catch clause
+
+		} else {
+
+			System.out.printf(USAGE_MESSAGE, F2C.class.getName());
+
+		} // end else
+
+	} // end main method
+
+} // end F2C Class
